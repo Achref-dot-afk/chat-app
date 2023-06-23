976398a9ce3739d1c373755d4ba3c2819eb41eb8
Achref Habli <120562438+Achref-dot-afk@users.noreply.github.com>
2023-06-23 18:54:58 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -24,20 +24,24 @@ app.get('/chat', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/chat.html'));
 });
 
+const users = {}
 // Handle incoming socket connections
 io.on('connection', (socket) => {
-  console.log('A user connected.');
+
 
   // Listen for incoming messages
-  socket.on('chat message', (message) => {
-    // Broadcast the message to all connected clients
-    socket.broadcast.emit('chat message', message);
+  socket.on('user-join',(username) => {
+    users[socket.id] = username ;
+    socket.broadcast.emit('user-connected',username);
+  });
+  socket.on('chat message', message => {
+    const obj = { message: message , username: users[socket.id] };
+    socket.broadcast.emit('chat message', obj);
+  });
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected',users[socket.id]);
   });
 
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('A user disconnected.');
-  });
 });
 // Start the server
 server.listen(3000, () => {
